acc2e516586e6302afb452b9918009805e4c3c73
Michael <mmcintyre@ebsco.com>
2020-07-11 23:18:43 -0400

    finished another

---
+++ most_popular_bite.py
@@ -0,0 +1,62 @@
+"""Checks community branch dir structure to see who submitted most
+   and what challenge is more popular by number of PRs"""
+from collections import Counter, namedtuple
+import os
+import urllib.request
+
+# prep
+tmp = os.getenv("TMP", "/tmp")
+tempfile = os.path.join(tmp, 'dirnames')
+urllib.request.urlretrieve(
+    'https://bites-data.s3.us-east-2.amazonaws.com/dirnames.txt',
+    tempfile
+)
+
+IGNORE = 'static templates data pybites bbelderbos hobojoe1848'.split()
+
+users, popular_challenges = Counter(), Counter()
+
+Stats = namedtuple('Stats', 'user challenge')
+
+
+# code
+
+def gen_files():
+    """Return a generator of dir names reading in tempfile
+
+       tempfile has this format:
+
+       challenge<int>/file_or_dir<str>,is_dir<bool>
+       03/rss.xml,False
+       03/tags.html,False
+       ...
+       03/mridubhatnagar,True
+       03/aleksandarknezevic,True
+
+       -> use last column to filter out directories (= True)
+    """
+    with open(tempfile) as f:
+        return (line.split(',')[0].lower()
+                for line in f.readlines()
+                if line.strip().endswith('True'))
+
+
+def diehard_pybites():
+    """Return a Stats namedtuple (defined above) that contains the user that
+       made the most PRs (ignoring the users in IGNORE) and a challenge tuple
+       of most popular challenge and the amount of PRs for that challenge.
+       Calling this function on the dataset (held tempfile) should return:
+       Stats(user='clamytoe', challenge=('01', 7))
+    """
+    for dir_ in gen_files():
+        ch, user = dir_.split('/')
+
+        if user in IGNORE:
+            continue
+
+        users[user] += 1
+        popular_challenges[ch] += 1
+
+    user = users.most_common(1)[0][0]
+    challenge = popular_challenges.most_common(1)[0]
+    return Stats(user=user, challenge=challenge)
